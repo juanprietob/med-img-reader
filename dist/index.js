2b5d5dfcc38a56edec1f18856adb2e2001167ae3
juanprietob <juanprietob@gmail.com>
2020-01-10 15:34:02 -0500

    ENH: Add index.js extending MedImgReader
    The extendend class is exported and implements additional methods

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,32 @@
-const Module = require('./MedImgReader');
-module.exports = Module();
+const {MedImgReaderBase, FS} = require('./MedImgReader');
+const MedImgReader = MedImgReaderBase.extend("MedImgReader", {
+    // __construct and __destruct are optional.  They are included
+    // in this example for illustration purposes.
+    // If you override __construct or __destruct, don't forget to
+    // call the parent implementation!
+    __construct: function() {
+        this.__parent.__construct.call(this);
+    },
+    __destruct: function() {
+        this.__parent.__destruct.call(this);
+    },
+    AddArrayBuffer: function(array_buffer, filename){
+    	var stream = FS.open(filename, 'w+');
+    	FS.write(stream, array_buffer, 0, $array_buffer.length, 0);
+    	FS.close(stream);
+    },
+    GetStream: function() {
+    	var filename = this.GetFilename();
+    	var stream = FS.open(filename);
+        return Promise.resolve(stream);
+    },
+    MakeDirectory(directory){
+    	try{
+    		FS.mkdir(directory);
+    	}catch(e){
+    		console.error(e);
+    	}
+    }
+});
+
+module.exports = MedImgReader;
