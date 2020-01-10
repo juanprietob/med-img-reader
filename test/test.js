b2f5bb6a5c32df254b015faf83b85e3df8a90671
juanprietob <juanprietob@gmail.com>
2020-01-10 15:36:13 -0500

    ENH: test script and data

--- test/test.js
+++ test/test.js
@@ -1,40 +1,145 @@
 
-if(process.argv.length < 4){
-	console.error("itkImageJS example, set random pixels to the image.");
-	console.error("Use mode: .nrrd or .nii files");
-	console.error(process.argv[0], process.argv[1], " inputImage outputImage");
-	return;
-}
+const Lab = require('@hapi/lab');
+const lab = exports.lab = Lab.script();
+const path = require('path');
+const MedImgReader = require('..');
 
-var {MedImgReader, FS} = require('../dist/index.js');
+lab.experiment("Test image-pad-resample", function(){
 
-console.log(FS)
-console.log(MedImgReader)
-const medImgReader = new MedImgReader();
-console.log(medImgReader)
+	// lab.test('returns true when a 2D image with four components is read.', function(){
 
-var inputImage = process.argv[2];
-var outputImage = process.argv[3];
+ //        const medImgReader = new MedImgReader();
 
-console.log("Input image: ", inputImage);
-console.log("Output image: ", outputImage);
+	// 	var inputImage = path.join(__dirname, 'gravitational.jpg')
+	// 	var outputImage = path.join(__dirname, 'gravitational_out.nrrd')
+
+	// 	console.log("Input image: ", inputImage);
+
+	// 	medImgReader.SetFilename(inputImage);
+	// 	medImgReader.ReadImage();
+
+	// 	inputImage = medImgReader.GetOutput();
+	// 	console.log("Image:", inputImage);
+		
+ //        const medImgReader2 = new MedImgReader();
+		
+	// 	medImgReader2.SetInput(inputImage);
+	// 	medImgReader2.SetFilename(outputImage);
+	// 	medImgReader2.WriteImage();
+
+	// 	console.log(medImgReader2.GetFilename());
+	// 	medImgReader2.GetStream()
+	// 	.then((wtf)=>{
+	// 		console.log(wtf)
+	// 	})
+		
+
+ //        return true;
+ //    });
+
+	lab.test('returns true when a 2D image with four components is read.', function(){
+
+        const medImgReader = new MedImgReader();
+
+		var inputImage = path.join(__dirname, 'brain.png')
+		var outputImage = path.join(__dirname, 'brain_out.nrrd')
+
+		console.log("Input image: ", inputImage);
+
+		medImgReader.SetFilename(inputImage);
+		medImgReader.ReadImage();
+
+		inputImage = medImgReader.GetOutput();
+		console.log("Image:", inputImage);
+		
+        const medImgReader2 = new MedImgReader();
+		
+		medImgReader2.SetInput(inputImage);
+		medImgReader2.SetFilename(outputImage);
+		medImgReader2.WriteImage();
+
+		medImgReader.GetStream()
+		.then((wtf)=>{
+			console.log(wtf)
+		});
+
+        return true;
+    });
+
+    lab.test('returns true when a 3D image with 1 components is read.', function(){
+
+        const medImgReader = new MedImgReader();
+
+		var inputImage = path.join(__dirname, 't1.nrrd')
+		var outputImage = path.join(__dirname, 't1_out.nii.gz')
+
+		console.log("Input image: ", inputImage);
+
+		medImgReader.SetFilename(inputImage);
+		medImgReader.ReadImage();
+
+		inputImage = medImgReader.GetOutput();
+		console.log("Image:", inputImage);
+		
+        const medImgReader2 = new MedImgReader();
+		
+		medImgReader2.SetInput(inputImage);
+		medImgReader2.SetFilename(outputImage);
+		medImgReader2.WriteImage();
 
 
-// console.log("Reading image...");
-medImgReader.SetFilename(inputImage);
-medImgReader.ReadImage();
+        return true;
+    });
 
-// medImgReader.SetDirectory(inputImage);
-// medImgReader.ReadDICOMDirectory();
+  //   lab.test('returns true when a dcm image compressed is read.', function(){
+
+  //       const medImgReader = new MedImgReader();
+
+		// var inputImage = path.join(__dirname, 'bmode.dcm')
+		// var outputImage = path.join(__dirname, 'bmode_out.nrrd')
+
+		// console.log("Input image: ", inputImage);
+
+		// medImgReader.SetFilename(inputImage);
+		// medImgReader.ReadImage();
+
+		// inputImage = medImgReader.GetOutput();
+		// console.log("Image:", inputImage);
+		
+  //       const medImgReader2 = new MedImgReader();
+		
+		// medImgReader2.SetInput(inputImage);
+		// medImgReader2.SetFilename(outputImage);
+		// medImgReader2.WriteImage();
 
 
-console.log("Spacing:", medImgReader.GetSpacing());
-console.log("Origin:", medImgReader.GetOrigin());
-console.log("Dimensions:", medImgReader.GetDimensions());
-console.log("Direction:", medImgReader.GetDirection());
-console.log("ComponentsPerPixel:", medImgReader.GetNumberOfComponentsPerPixel());
-console.log("ImageBuffer:", medImgReader.GetImageBuffer());
+  //       return true;
+  //   });
 
-console.log("Writing image...");
-medImgReader.SetOutputFilename(outputImage);
-medImgReader.WriteImage();
+  //   lab.test('returns true when a dcm series directory read.', function(){
+
+  //       const medImgReader = new MedImgReader();
+  //       //https://www.dicomlibrary.com?requestType=WADO&studyUID=1.2.826.0.1.3680043.8.1055.1.20111102150758591.92402465.76095170&manage=1b9baeb16d2aeba13bed71045df1bc65
+		// var inputDirectory = path.join(__dirname, 'series-000001')
+		// var outputImage = path.join(__dirname, 'series-000001.nrrd')
+
+		// console.log("Input directory: ", inputDirectory);
+
+		// medImgReader.SetDirectory(inputDirectory);
+		// medImgReader.ReadDICOMDirectory();
+
+		// inputImage = medImgReader.GetOutput();
+		// console.log("Image:", inputImage);
+		
+  //       const medImgReader2 = new MedImgReader();
+		
+		// medImgReader2.SetInput(inputImage);
+		// medImgReader2.SetFilename(outputImage);
+		// medImgReader2.WriteImage();
+
+
+  //       return true;
+  //   });
+    
+
+})
